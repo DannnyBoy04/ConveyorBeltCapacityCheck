32580070cbe2d5eb98976087d9ba0c89bf7a5c62
Dánjal Leitisstein Olsen <danjal.olsen04@gmail.com>
2025-09-14 18:38:00 +0200

    Added error handling, updated README

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 **Conveyor Belt Capacity Check**
 
-A basic C program which checks, if the motors in a conveyor belt are capable of carrying the user-inputted weight of packages.
+A basic C program, which checks if the motors in a conveyor belt are capable of carrying the user-inputted weight of packages.
+It also has error-handling for if the user inputs the wrong data-type, e.g. a letter when an int or double was expected.
--- main.c
+++ main.c
@@ -4,24 +4,43 @@
 
 // Defining relevant variables.
 // Initializes motorCapacity to 5.6, as each motor can carry that weight in kg.
-int motorCount;
+double motorCount;
 double motorCapacity = 5.6;
 double totalPackageWeight;
 
-// Prompts the user for how many motors will be carrying the packages.
-// Prompts the user for the weight of those packages.
-// Calculates if the motors are able to carry the packages.
-// Displays the answer to the user.
+// Prompts the user for inputting the number of motors and total package weight.
+// Displays an error if the user inputs the wrong datatype, such as a char.
+// Calculates if the inputted number of motors can carry the packages.
 int main(void) {
   printf("How many motors will be carrying the packages?\n");
-  scanf("%d", &motorCount);
 
-  printf("What is the total weight of the packages?\n");
-  scanf("%lf", &totalPackageWeight);
+  // Defines an integer which is 1 if the input
+  // from scanf is of the correct datatype.
+  int validMotorCountInput = scanf("%lf", &motorCount);
 
-  if (totalPackageWeight / motorCount <= motorCapacity) {
-    printf("The motors can carry the packages!");
+  // Checks if the user has inputted the correct datatype.
+  if (validMotorCountInput == 1) {
+    printf("What is the total weight of the packages?\n");
+
+    // Defines an integer which is 1 of the input
+    // from scanf is of the correct datatype.
+    int validPackageWeightInput = scanf("%lf", &totalPackageWeight);
+
+    // Checks if the user has inputted the correct datatype and calculates
+    // the ratio between package weight and how much the motors can carry.
+    if (validPackageWeightInput == 1 &&
+        totalPackageWeight / motorCount <= motorCapacity) {
+      printf("The motors can carry the packages!");
+      // If the datatype is correct, but
+    } else if (validPackageWeightInput == 1 &&
+               totalPackageWeight / motorCount > motorCapacity) {
+      printf("The motors cannot carry the packages!");
+      // If none of the above are true, then the datatype must be incorrect.
+    } else {
+      printf("Error: wrong data format inputted!");
+    }
+    // If none of the above are true, then the datatype must be incorrect.
   } else {
-    printf("The motors cannot carry the packages!");
+    printf("Error: wrong data format inputted!");
   }
 }
